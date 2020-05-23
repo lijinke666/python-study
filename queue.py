c2476eb130555aba39a216e4d1e72d398613ff54
Jinke.Li <a1231236677287@163.com>
2020-05-23 22:31:18 +0800

    consumer

--- queue.py
+++ queue.py
@@ -22,7 +22,28 @@ class Producer(Thread):
     while True:
       a = random.randint(0, 10)
       b = random.randint(0, 10)
+      print(f'Producer: {a}, {b}')
       self.queue.put((a, b))
 
       # 等待两秒
       time.sleep(2)
+
+class Consumer(Thread):
+  def __init__(self, queue):
+    super.__init__();
+    self.queue = queue
+
+  def run(self):
+    while True:
+      # 从队列里面取数据, 如果队列为空一直等待
+      tuple = self.queue.get(block= True)
+      tupleSum = sum(tuple)
+      print(f'Consumer: {tuple[0]} , {tuple[1]} = {tupleSum}')
+      time.sleep(random.randint(0, 10))
+
+queue = Queue()
+producer = Producer(queue)
+consumer = Consumer(queue)
+
+producer.start()
+consumer.start()
